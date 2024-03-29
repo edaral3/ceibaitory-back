312f19f3e89b773abfbdf1455fde7db90fd35ac0
Edgar Aldana <arnoldo201@hotmail.com>
2024-03-29 09:41:53 -0600

    wip

--- src/config/config.ts
+++ src/config/config.ts
@@ -4,15 +4,15 @@ dotenv.config()
 const config = {
   db: {
     mongo: {
-      host: process.env.DB_MONGO_HOST
+      host: process.env.DB_MONGO_HOST || ""
     },
     redis: {
-      host: process.env.DB_REDIS_HOST,
-      password: process.env.DB_REDIS_PASSWORD,
-      port: process.env.DB_REDIS_PORT
+      host: process.env.DB_REDIS_HOST || "",
+      password: process.env.DB_REDIS_PASSWORD || "",
+      port: process.env.DB_REDIS_PORT || ""
     }
   },
-  secret: process.env.SECRET
+  secret: process.env.SECRET || ""
 }
 
 export default config
