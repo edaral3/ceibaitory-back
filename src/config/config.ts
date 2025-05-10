93698e1b03f7ba5b1f6a1df8b9f96595ed0a55c8
Edgar Arnoldo Aldana Arriola <138614706+eaaldana@users.noreply.github.com>
2025-05-10 08:42:26 -0600

    redis removed

--- src/config/config.ts
+++ src/config/config.ts
@@ -6,11 +6,6 @@ const config = {
     mongo: {
       host: process.env.DB_MONGO_HOST || ""
     },
-    redis: {
-      host: process.env.DB_REDIS_HOST || "",
-      password: process.env.DB_REDIS_PASSWORD || "",
-      port: process.env.DB_REDIS_PORT || ""
-    }
   },
   secret: process.env.SECRET || "" 
 }
