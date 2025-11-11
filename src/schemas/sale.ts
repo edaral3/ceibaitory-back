d8bb8465c2f1be7051057bce3037b89d55f9157c
Edgar Arnoldo Aldana Arriola <138614706+eaaldana@users.noreply.github.com>
2025-11-11 14:25:14 -0600

    Update sale.ts

--- src/schemas/sale.ts
+++ src/schemas/sale.ts
@@ -5,8 +5,8 @@ import trade from './trade'
 const schema = Joi.object({
   total: Joi.number().allow(0).min(0).required().messages(messages.validationStringMessages),
   description: Joi.string().max(500).trim().allow('').messages(messages.validationStringMessages),
-  clientName: Joi.string().max(500).trim().allow('').messages(messages.validationStringMessages),
-  clientNit: Joi.string().max(500).trim().allow('').messages(messages.validationStringMessages),
+  clientName: Joi.optional(),
+  clientNit: Joi.optional(),
   date: Joi.allow(),
   direction: Joi.allow(),
   products: Joi.array().items(trade),
